07636c960d171344c2e6bf51e244373e11d84965
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2018-08-02 13:18:21 +0200

    Removing import ptvsd

--- swimming_dem/use_cases/PFEMFluid_Element/Two_Way/Small_Box_Lagrangian_TW/source/KratosSwimmingDEMPFEM.py
+++ swimming_dem/use_cases/PFEMFluid_Element/Two_Way/Small_Box_Lagrangian_TW/source/KratosSwimmingDEMPFEM.py
@@ -7,7 +7,6 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 import sys
-import ptvsd
 
 # Kratos
 from KratosMultiphysics import *
