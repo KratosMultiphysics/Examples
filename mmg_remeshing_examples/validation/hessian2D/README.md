e9fb84de08b0611b859f6a1405ece8ffd7845db7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-03-30 17:20:10 +0100

    Update 2D hessian

--- mmg_remeshing_examples/validation/hessian2D/README.md
+++ mmg_remeshing_examples/validation/hessian2D/README.md
@@ -2,7 +2,7 @@
 
 **Author:** Vicente Mataix Ferrándiz
 
-**Kratos version:** 6.1-dev
+**Kratos version:** 7.1
 
 **Source files:** [Hessian 2D](https://github.com/KratosMultiphysics/Examples/tree/master/mmg_remeshing_examples/validation/hessian2D/source)
 
--- mmg_remeshing_examples/validation/hessian2D/source/test_hessian.py
+++ mmg_remeshing_examples/validation/hessian2D/source/test_hessian.py
@@ -9,16 +9,15 @@ model = KratosMultiphysics.Model()
 main_model_part = model.CreateModelPart("MainModelPart")
 main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 2)
 
-# We add the variables needed 
+# We add the variables needed
 main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)
-main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_H)
 main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
 
 # We import the model main_model_part
 KratosMultiphysics.ModelPartIO("2D_hessian_test").ReadModelPart(main_model_part)
 
 # Calculate NODAL_H
-find_nodal_h = KratosMultiphysics.FindNodalHProcess(main_model_part)
+find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(main_model_part)
 find_nodal_h.Execute()
 
 for node in main_model_part.Nodes:
@@ -27,24 +26,24 @@ for node in main_model_part.Nodes:
 
 # We calculate the gardient of the distance variable
 metric_param = KratosMultiphysics.Parameters(
-    """{
-        "hessian_strategy_parameters"              :{
-                "estimate_interpolation_error"     : false,
-                "interpolation_error"              : 0.004,
-                "mesh_dependent_constant"          : 0.28125
-        },
-        "minimal_size"                      : 0.005,
-        "maximal_size"                      : 1.0,
-        "enforce_current"                   : false,
-        "anisotropy_remeshing"              : true,
-        "anisotropy_parameters":{
-            "reference_variable_name"          : "DISTANCE",
-            "hmin_over_hmax_anisotropic_ratio" : 0.15,
-            "boundary_layer_max_distance"      : 1.0,
-            "interpolation"                    : "Linear"
-        }
-    }"""
-    )
+"""{
+    "hessian_strategy_parameters"              :{
+            "estimate_interpolation_error"     : false,
+            "interpolation_error"              : 0.004,
+            "mesh_dependent_constant"          : 0.28125
+    },
+    "minimal_size"                      : 0.005,
+    "maximal_size"                      : 1.0,
+    "enforce_current"                   : false,
+    "anisotropy_remeshing"              : true,
+    "anisotropy_parameters":{
+        "reference_variable_name"          : "DISTANCE",
+        "hmin_over_hmax_anisotropic_ratio" : 0.15,
+        "boundary_layer_max_distance"      : 1.0,
+        "interpolation"                    : "Linear"
+    }
+}"""
+)
 local_gradient = MeshingApplication.ComputeHessianSolMetricProcess2D(main_model_part, KratosMultiphysics.DISTANCE, metric_param)
 local_gradient.Execute()
 
@@ -58,18 +57,19 @@ from gid_output_process import GiDOutputProcess
 gid_output = GiDOutputProcess(main_model_part,
                             "gid_output",
                             KratosMultiphysics.Parameters("""
-                                {
-                                    "result_file_configuration" : {
-                                        "gidpost_flags": {
-                                            "GiDPostMode": "GiD_PostBinary",
-                                            "WriteDeformedMeshFlag": "WriteUndeformed",
-                                            "WriteConditionsFlag": "WriteConditions",
-                                            "MultiFileFlag": "SingleFile"
-                                        },        
-                                        "nodal_results"       : ["DISTANCE","NODAL_H"]
-                                    }
+                            {
+                                "result_file_configuration" : {
+                                    "gidpost_flags": {
+                                        "GiDPostMode": "GiD_PostBinary",
+                                        "WriteDeformedMeshFlag": "WriteUndeformed",
+                                        "WriteConditionsFlag": "WriteConditions",
+                                        "MultiFileFlag": "SingleFile"
+                                    },
+                                    "nodal_results"               : ["DISTANCE"],
+                                    "nodal_nonhistorical_results" : ["NODAL_H"]
                                 }
-                                """)
+                            }
+                            """)
                             )
 
 gid_output.ExecuteInitialize()
@@ -77,5 +77,24 @@ gid_output.ExecuteBeforeSolutionLoop()
 gid_output.ExecuteInitializeSolutionStep()
 gid_output.PrintOutput()
 gid_output.ExecuteFinalizeSolutionStep()
-gid_output.ExecuteFinalize()   
+gid_output.ExecuteFinalize()
 
+# Finally we export to VTK
+vtk_settings = KratosMultiphysics.Parameters("""{
+    "model_part_name"                    : "MainModelPart",
+    "file_format"                        : "ascii",
+    "output_precision"                   : 7,
+    "output_control_type"                : "step",
+    "output_frequency"                   : 1.0,
+    "output_sub_model_parts"             : false,
+    "save_output_files_in_folder"        : false,
+    "nodal_solution_step_data_variables" : ["DISTANCE"],
+    "nodal_data_value_variables"         : ["NODAL_H"],
+    "nodal_flag"                         : [],
+    "element_data_value_variables"       : [],
+    "condition_data_value_variables"     : [],
+    "gauss_point_variables"              : []
+}""")
+
+vtk_io = KratosMultiphysics.VtkOutput(main_model_part, vtk_settings)
+vtk_io.PrintOutput()
