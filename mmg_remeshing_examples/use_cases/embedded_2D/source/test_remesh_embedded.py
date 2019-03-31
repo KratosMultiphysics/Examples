16679e38f5c0ff8175bd9430522053263a3f8357
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-03-31 14:05:47 +0200

    Update standalone script

--- mmg_remeshing_examples/use_cases/embedded_2D/source/test_remesh_embedded.py
+++ mmg_remeshing_examples/use_cases/embedded_2D/source/test_remesh_embedded.py
@@ -4,20 +4,19 @@ import KratosMultiphysics
 import KratosMultiphysics.MeshingApplication as MeshingApplication
 import KratosMultiphysics.FluidDynamicsApplication as CFDApplication
 
-main_model_part = KratosMultiphysics.ModelPart("MainModelPart")
+current_model = KratosMultiphysics.Model()
+main_model_part = current_model.CreateModelPart("MainModelPart")
 main_model_part.ProcessInfo.SetValue(KratosMultiphysics.DOMAIN_SIZE, 2)
 
-# We add the variables needed 
+# We add the variables needed
 main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE)
 main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.DISTANCE_GRADIENT)
-main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_H)
-main_model_part.AddNodalSolutionStepVariable(KratosMultiphysics.NODAL_AREA)
 
 # We import the model main_model_part
 KratosMultiphysics.ModelPartIO("test_embedded_2D_refined").ReadModelPart(main_model_part)
 #KratosMultiphysics.ModelPartIO("test_embedded_2D_coarse").ReadModelPart(main_model_part)
 
-find_nodal_h = KratosMultiphysics.FindNodalHProcess(main_model_part)
+find_nodal_h = KratosMultiphysics.FindNodalHNonHistoricalProcess(main_model_part)
 find_nodal_h.Execute()
 
 ## Circle distance function
@@ -33,23 +32,23 @@ local_gradient = KratosMultiphysics.ComputeNodalGradientProcess2D(main_model_par
 local_gradient.Execute()
 
 # We set to zero the metric
-ZeroVector = KratosMultiphysics.Vector(3) 
+ZeroVector = KratosMultiphysics.Vector(3)
 ZeroVector[0] = 0.0
 ZeroVector[1] = 0.0
 ZeroVector[2] = 0.0
 for node in main_model_part.Nodes:
-    node.SetValue(MeshingApplication.MMG_METRIC, ZeroVector)
-            
+    node.SetValue(MeshingApplication.METRIC_TENSOR_2D, ZeroVector)
+
 # We define a metric using the ComputeLevelSetSolMetricProcess
 level_set_param = KratosMultiphysics.Parameters("""
                         {
-                            "minimal_size"                         : 0.25, 
-                            "enforce_current"                      : false, 
-                            "anisotropy_remeshing"                 : true, 
-                            "anisotropy_parameters": 
+                            "minimal_size"                         : 0.25,
+                            "enforce_current"                      : false,
+                            "anisotropy_remeshing"                 : true,
+                            "anisotropy_parameters":
                             {
-                                "hmin_over_hmax_anisotropic_ratio"      : 0.1, 
-                                "boundary_layer_max_distance"           : 1.0, 
+                                "hmin_over_hmax_anisotropic_ratio"      : 0.1,
+                                "boundary_layer_max_distance"           : 1.0,
                                 "interpolation"                         : "Exponential"
                             }
                         }
@@ -81,7 +80,7 @@ gid_output = GiDOutputProcess(main_model_part,
                                             "WriteDeformedMeshFlag": "WriteUndeformed",
                                             "WriteConditionsFlag": "WriteConditions",
                                             "MultiFileFlag": "SingleFile"
-                                        },        
+                                        },
                                         "nodal_results"       : ["DISTANCE","DISTANCE_GRADIENT"],
                                         "nodal_nonhistorical_results"       : ["ANISOTROPIC_RATIO"]
                                     }
@@ -94,5 +93,23 @@ gid_output.ExecuteBeforeSolutionLoop()
 gid_output.ExecuteInitializeSolutionStep()
 gid_output.PrintOutput()
 gid_output.ExecuteFinalizeSolutionStep()
-gid_output.ExecuteFinalize()   
+gid_output.ExecuteFinalize()
 
+# Finally we export to VTK
+vtk_settings = KratosMultiphysics.Parameters("""{
+    "model_part_name"                    : "MainModelPart",
+    "file_format"                        : "ascii",
+    "output_precision"                   : 7,
+    "output_control_type"                : "step",
+    "output_frequency"                   : 1.0,
+    "output_sub_model_parts"             : false,
+    "save_output_files_in_folder"        : false,
+    "nodal_solution_step_data_variables" : ["DISTANCE","DISTANCE_GRADIENT"],
+    "nodal_data_value_variables"         : ["ANISOTROPIC_RATIO"],
+    "element_data_value_variables"       : [],
+    "condition_data_value_variables"     : [],
+    "gauss_point_variables"              : []
+}""")
+
+vtk_io = KratosMultiphysics.VtkOutput(main_model_part, vtk_settings)
+vtk_io.PrintOutput()
