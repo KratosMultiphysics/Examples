8dca7e13023a99d7110932a7b11b35d808927eec
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-27 13:01:24 +0200

    Update script

--- mmg_remeshing_examples/use_cases/beam_spr/sources/MainKratos.py
+++ mmg_remeshing_examples/use_cases/beam_spr/sources/MainKratos.py
@@ -5,7 +5,7 @@ import KratosMultiphysics as KM
 import KratosMultiphysics.MeshingApplication as MA
 import KratosMultiphysics.StructuralMechanicsApplication as SMA
 
-import adaptative_remeshing_structural_mechanics_analysis as Analysis
+from KratosMultiphysics.StructuralMechanicsApplication.adaptative_remeshing_structural_mechanics_analysis import AdaptativeRemeshingStructuralMechanicsAnalysis
 
 ## Import define_output
 with open("ProjectParameters.json",'r') as parameter_file:
@@ -13,6 +13,6 @@ with open("ProjectParameters.json",'r') as parameter_file:
 
 # Creating the test
 model = KM.Model()
-analysis = Analysis.AdaptativeRemeshingStructuralMechanicsAnalysis(model, ProjectParameters)
+analysis = AdaptativeRemeshingStructuralMechanicsAnalysis(model, ProjectParameters)
 KM.Logger.GetDefaultOutput().SetSeverity(KM.Logger.Severity.INFO)
 analysis.Run()
