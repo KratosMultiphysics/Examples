3276d0d092fb1c71a1edb33c415bc579dec001e2
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-08-20 12:04:55 +0200

    Adding case (WIP)

---
+++ mmg_remeshing_examples/use_cases/contact_hessian/source/MainKratos.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+# Importing Kratos
+import KratosMultiphysics as KM
+
+from KratosMultiphysics.ContactStructuralMechanicsApplication.adaptative_remeshing_contact_structural_mechanics_analysis import AdaptativeRemeshingContactStructuralMechanicsAnalysis
+
+## Import define_output
+with open("ProjectParameters.json",'r') as parameter_file:
+    ProjectParameters = KM.Parameters(parameter_file.read())
+
+# Creating the test
+model = KM.Model()
+analysis = AdaptativeRemeshingContactStructuralMechanicsAnalysis(model, ProjectParameters)
+analysis.Run()
