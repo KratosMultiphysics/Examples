86d7be95dd60de56e214f85533c31e6d7cb272ba
riccardotosi <rtosi@cimne.upc.edu>
2020-08-31 14:12:45 +0200

    Add bash file for PyCOMPSs

---
+++ multilevel_monte_carlo/validation/poisson_equation/asynchronous_multilevel_monte_carlo/run_with_pycompss.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+path_to_examples_folder=$(pwd)
+
+runcompss \
+    --lang=python \
+    --python_interpreter=python3 \
+    --pythonpath=$path_to_examples_folder/ \
+    ./run_mlmc_Kratos.py
