6e46c6c5742ddca3bd1b934b32bc5048f83196d7
riccardotosi <rtosi@cimne.upc.edu>
2020-11-25 17:00:03 +0100

    Rename main script

--- multilevel_monte_carlo/use_cases/wind_engineering_rectangle/stochastic_MC/source/run.sh
+++ multilevel_monte_carlo/use_cases/wind_engineering_rectangle/stochastic_MC/source/run.sh
@@ -4,4 +4,4 @@ runcompss \
     --lang=python \
     --python_interpreter=python3 \
     --pythonpath=/PATH/TO/THIS/FOLDER/ \
-    ./run_xmc_mc.py problem_settings/parameters_xmc_asynchronous_mc_rectangle_cylinder_Fractional.json
+    ./run_mc_Kratos.py problem_settings/parameters_xmc_asynchronous_mc_rectangle_cylinder_Fractional.json
