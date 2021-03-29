d8fbb567f3ec49119f0c5c233993dc1c5bc5e68d
riccardotosi <rtosi@cimne.upc.edu>
2021-03-29 09:51:20 +0200

    Update for new kratos release

--- multilevel_monte_carlo/use_cases/wind_engineering_CAARC/steady_inlet_ensemble_average/source/run_mc_Kratos.py
+++ multilevel_monte_carlo/use_cases/wind_engineering_CAARC/steady_inlet_ensemble_average/source/run_mc_Kratos.py
@@ -6,12 +6,12 @@ import json
 import time
 import pickle
 
-# Import Kratos, XMC, PyCOMPSs API
+# Import XMC, Kratos, COMPSs
 import KratosMultiphysics
-import KratosMultiphysics.MultilevelMonteCarloApplication
 import xmc
 import xmc.methodDefs_momentEstimator.computeCentralMoments as mdccm
-from exaqute.ExaquteTaskLocal import *
+from exaqute import *   # to execute with runcompss
+
 
 if __name__ == "__main__":
 
@@ -22,83 +22,98 @@ if __name__ == "__main__":
 
     # read parameters
     with open(parametersPath,'r') as parameter_file:
-            parameters = json.load(parameter_file)
+        parameters = json.load(parameter_file)
 
-    # add path of the problem folder to python path
-    problem_id = parameters["solverWrapperInputDictionary"]["problemId"]
-    sys.path.append(os.path.join("..","xmc","classDefs_solverWrapper","problemDefs_KratosMultiphysics",problem_id))
-
+    # Prepare MonteCarloIndexInputDictionary
+    me = "xmc.momentEstimator.MomentEstimator"
+    me_settings = {"indexSetDimension": 0, "order": 5, "updatedPowerSums":"xmc.methodDefs_momentEstimator.updatePowerSums.updatePowerSumsOrder10Dimension0", "centralMomentComputer":"xmc.methodDefs_momentEstimator.computeCentralMoments.centralMomentWrapper", "centralMomentErrorComputer":"xmc.methodDefs_momentEstimator.computeErrorEstimation.centralMomentErrorWrapper"}
+    cme = "xmc.momentEstimator.CombinedMomentEstimator"
+    cme_settings = {"indexSetDimension": 0, "order": 5, "updatedPowerSums":"xmc.methodDefs_momentEstimator.updateCombinedPowerSums.updatePowerSumsOrder10Dimension0", "centralMomentComputer":"xmc.methodDefs_momentEstimator.computeCentralMoments.centralMomentWrapper", "centralMomentErrorComputer":"xmc.methodDefs_momentEstimator.computeErrorEstimation.centralMomentErrorWrapper"}
+    for _ in range (0,18738):
+        parameters["monteCarloIndexInputDictionary"]["qoiEstimator"].append(me)
+        parameters["monteCarloIndexInputDictionary"]["qoiEstimatorInputDictionary"].append(me_settings)
+    for _ in range (0,18738):
+        parameters["monteCarloIndexInputDictionary"]["qoiEstimator"].append(cme)
+        parameters["monteCarloIndexInputDictionary"]["qoiEstimatorInputDictionary"].append(cme_settings)
+    # SolverWrapper
+    parameters["solverWrapperInputDictionary"]["qoiEstimator"] = parameters["monteCarloIndexInputDictionary"]["qoiEstimator"]
     # SampleGenerator
     samplerInputDictionary = parameters["samplerInputDictionary"]
-    samplerInputDictionary['randomGeneratorInputDictionary'] = parameters["randomGeneratorInputDictionary"]
-    samplerInputDictionary['solverWrapperInputDictionary'] = parameters["solverWrapperInputDictionary"]
-
+    samplerInputDictionary["randomGeneratorInputDictionary"] = parameters[
+        "randomGeneratorInputDictionary"
+    ]
+    samplerInputDictionary["solverWrapperInputDictionary"] = parameters[
+        "solverWrapperInputDictionary"
+    ]
     # MonteCarloIndex
     monteCarloIndexInputDictionary = parameters["monteCarloIndexInputDictionary"]
     monteCarloIndexInputDictionary["samplerInputDictionary"] = samplerInputDictionary
-
-    # Moment Estimators
-    qoiEstimatorInputDictionary = parameters["qoiEstimatorInputDictionary"]
-    combinedEstimatorInputDictionary = parameters["combinedEstimatorInputDictionary"]
-    costEstimatorInputDictionary = parameters["costEstimatorInputDictionary"]
-    # qoi estimators
-    monteCarloIndexInputDictionary["qoiEstimator"] = [monteCarloIndexInputDictionary["qoiEstimator"][0] for _ in range (0,parameters["solverWrapperInputDictionary"]["numberQoI"])]
-    monteCarloIndexInputDictionary["qoiEstimatorInputDictionary"] = [qoiEstimatorInputDictionary]*parameters["solverWrapperInputDictionary"]["numberQoI"]
-    # combined estimators
-    monteCarloIndexInputDictionary["combinedEstimator"] = [monteCarloIndexInputDictionary["combinedEstimator"][0] for _ in range (0,parameters["solverWrapperInputDictionary"]["numberCombinedQoi"])]
-    monteCarloIndexInputDictionary["combinedEstimatorInputDictionary"] = [combinedEstimatorInputDictionary]*parameters["solverWrapperInputDictionary"]["numberCombinedQoi"]
-    # cost estimator
-    monteCarloIndexInputDictionary["costEstimatorInputDictionary"] = costEstimatorInputDictionary
-
     # MonoCriterion
     criteriaArray = []
     criteriaInputs = []
-    for monoCriterion in (parameters["monoCriteriaInpuctDictionary"]):
-        criteriaArray.append(xmc.monoCriterion.MonoCriterion(\
-            parameters["monoCriteriaInpuctDictionary"][monoCriterion]["criteria"],\
-            parameters["monoCriteriaInpuctDictionary"][monoCriterion]["tolerance"]))
-        criteriaInputs.append([parameters["monoCriteriaInpuctDictionary"][monoCriterion]["input"]])
-
+    for monoCriterion in parameters["monoCriteriaInputDictionary"]:
+        criteriaArray.append(
+            xmc.monoCriterion.MonoCriterion(
+                parameters["monoCriteriaInputDictionary"][monoCriterion]["criteria"],
+                parameters["monoCriteriaInputDictionary"][monoCriterion]["tolerance"],
+            )
+        )
+        criteriaInputs.append(
+            [parameters["monoCriteriaInputDictionary"][monoCriterion]["input"]]
+        )
     # MultiCriterion
-    multiCriterionInputDictionary=parameters["multiCriterionInputDictionary"]
+    multiCriterionInputDictionary = parameters["multiCriterionInputDictionary"]
     multiCriterionInputDictionary["criteria"] = criteriaArray
     multiCriterionInputDictionary["inputsForCriterion"] = criteriaInputs
     criterion = xmc.multiCriterion.MultiCriterion(**multiCriterionInputDictionary)
-
     # ErrorEstimator
-    statErrorEstimator = xmc.errorEstimator.ErrorEstimator(**parameters["errorEstimatorInputDictionary"])
-
+    statErrorEstimator = xmc.errorEstimator.ErrorEstimator(
+        **parameters["errorEstimatorInputDictionary"]
+    )
     # HierarchyOptimiser
-    hierarchyCostOptimiser = xmc.hierarchyOptimiser.HierarchyOptimiser(**parameters["hierarchyOptimiserInputDictionary"])
-
+    hierarchyCostOptimiser = xmc.hierarchyOptimiser.HierarchyOptimiser(
+        **parameters["hierarchyOptimiserInputDictionary"]
+    )
     # EstimationAssembler
-    if "expectationAssembler" in parameters["estimationAssemblerInputDictionary"].keys():
-        expectationAssembler = xmc.estimationAssembler.EstimationAssembler(**parameters["estimationAssemblerInputDictionary"]["expectationAssembler"])
+    if (
+        "expectationAssembler"
+        in parameters["estimationAssemblerInputDictionary"].keys()
+    ):
+        expectationAssembler = xmc.estimationAssembler.EstimationAssembler(
+            **parameters["estimationAssemblerInputDictionary"]["expectationAssembler"]
+        )
     if "varianceAssembler" in parameters["estimationAssemblerInputDictionary"].keys():
-        varianceAssembler = xmc.estimationAssembler.EstimationAssembler(**parameters["estimationAssemblerInputDictionary"]["varianceAssembler"])
-
+        varianceAssembler = xmc.estimationAssembler.EstimationAssembler(
+            **parameters["estimationAssemblerInputDictionary"]["varianceAssembler"]
+        )
     # MonteCarloSampler
     monteCarloSamplerInputDictionary = parameters["monteCarloSamplerInputDictionary"]
-    monteCarloSamplerInputDictionary["indexConstructorDictionary"] = monteCarloIndexInputDictionary
-    monteCarloSamplerInputDictionary["assemblers"] =  [expectationAssembler,varianceAssembler]
+    monteCarloSamplerInputDictionary[
+        "indexConstructorDictionary"
+    ] = monteCarloIndexInputDictionary
+    monteCarloSamplerInputDictionary["assemblers"] = [
+        expectationAssembler,
+        varianceAssembler,
+    ]
     monteCarloSamplerInputDictionary["errorEstimators"] = [statErrorEstimator]
-    mcSampler = xmc.monteCarloSampler.MonteCarloSampler(**monteCarloSamplerInputDictionary)
-
+    mcSampler = xmc.monteCarloSampler.MonteCarloSampler(
+        **monteCarloSamplerInputDictionary
+    )
     # XMCAlgorithm
     XMCAlgorithmInputDictionary = parameters["XMCAlgorithmInputDictionary"]
     XMCAlgorithmInputDictionary["monteCarloSampler"] = mcSampler
     XMCAlgorithmInputDictionary["hierarchyOptimiser"] = hierarchyCostOptimiser
     XMCAlgorithmInputDictionary["stoppingCriterion"] = criterion
-
     algo = xmc.XMCAlgorithm(**XMCAlgorithmInputDictionary)
 
     time_start = time.time()
+    algo = xmc.XMCAlgorithm(**XMCAlgorithmInputDictionary)
     if (parameters["solverWrapperInputDictionary"]["asynchronous"] is True):
         algo.runAsynchronousXMC()
     else:
         algo.runXMC()
     time_end = time.time()
-    print("[SCREENING] time to solution:",time_end-time_start)
+    print("[SCREENING] computational time:",time_end-time_start)
 
     ########################################################################################################################################################################################################
     ########################################################################################################################################################################################################
@@ -152,6 +167,26 @@ if __name__ == "__main__":
             h2 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderTwoDimensionZero(S1,S2,sample_counter)))
             qoi_dict["qoi_id_"+str(qoi_counter)]["index_"+str(index)] = {"qoi_id":qoi_counter, "index": index, "instances": sample_counter, "S1": S1, "S2": S2, "S3": S3, "S4": S4, "S5": S5, "S6": S6, "S7": S7, "S8": S8, "S9": S9, "S10": S10, "h1": h1, "h2": h2,"type":"time_averaged_quantity","tag":"drag_force_x"}
 
+    # save time averaged base moment
+    for qoi_counter in range (1,2):
+        qoi_dict["qoi_id_"+str(qoi_counter)] = {"index_"+str(index): {} for index in range (len(algo.monteCarloSampler.indices))}
+        for index in range (len(algo.monteCarloSampler.indices)):
+            algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter] = get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter])
+            sample_counter = algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter]._sampleCounter
+            S1 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[0][0]))
+            S2 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[1][0]))
+            S3 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[2][0]))
+            S4 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[3][0]))
+            S5 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[4][0]))
+            S6 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[5][0]))
+            S7 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[6][0]))
+            S8 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[7][0]))
+            S9 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[8][0]))
+            S10 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[9][0]))
+            h1 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderOneDimensionZero(S1,sample_counter)))
+            h2 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderTwoDimensionZero(S1,S2,sample_counter)))
+            qoi_dict["qoi_id_"+str(qoi_counter)]["index_"+str(index)] = {"qoi_id":qoi_counter, "index": index, "instances": sample_counter, "S1": S1, "S2": S2, "S3": S3, "S4": S4, "S5": S5, "S6": S6, "S7": S7, "S8": S8, "S9": S9, "S10": S10, "h1": h1, "h2": h2,"type":"time_averaged_quantity","tag":"base_moment_z"}
+
     # save time averaged pressure field
     for node in current_model.GetModelPart(model_part_of_interest).Nodes:
         qoi_counter = qoi_counter + 1
@@ -174,7 +209,7 @@ if __name__ == "__main__":
             qoi_dict["qoi_id_"+str(qoi_counter)]["index_"+str(index)] = {"qoi_id":qoi_counter, "index": index, "instances": sample_counter, "S1": S1, "S2": S2, "S3": S3, "S4": S4, "S5": S5, "S6": S6, "S7": S7, "S8": S8, "S9": S9, "S10": S10, "h1": h1, "h2": h2,"type":"time_averaged_quantity","tag":"pressure","node_id":node.Id,"node_coordinates":[node.X,node.Y,node.Z]}
 
     # save time series drag
-    for qoi_counter in range (parameters["solverWrapperInputDictionary"]["numberQoI"],parameters["solverWrapperInputDictionary"]["numberQoI"]+1):
+    for qoi_counter in range (parameters["solverWrapperInputDictionary"]["numberMomentEstimator"],parameters["solverWrapperInputDictionary"]["numberMomentEstimator"]+1):
         qoi_dict["qoi_id_"+str(qoi_counter)] = {"index_"+str(index): {} for index in range (len(algo.monteCarloSampler.indices))}
         for index in range (len(algo.monteCarloSampler.indices)):
             algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter] = get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter])
@@ -192,6 +227,26 @@ if __name__ == "__main__":
             h1 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderOneDimensionZero(S1,sample_counter)))
             h2 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderTwoDimensionZeroBiased(S1,S2,sample_counter)))
             qoi_dict["qoi_id_"+str(qoi_counter)]["index_"+str(index)] = {"qoi_id":qoi_counter, "index": index, "instances": sample_counter, "S1": S1, "S2": S2, "S3": S3, "S4": S4, "S5": S5, "S6": S6, "S7": S7, "S8": S8, "S9": S9, "S10": S10, "h1": h1, "h2": h2,"type":"time_series_quantity","tag":"drag_force_x"}
+
+    # save time series base moment
+    for qoi_counter in range (parameters["solverWrapperInputDictionary"]["numberMomentEstimator"]+1,parameters["solverWrapperInputDictionary"]["numberMomentEstimator"]+2):
+        qoi_dict["qoi_id_"+str(qoi_counter)] = {"index_"+str(index): {} for index in range (len(algo.monteCarloSampler.indices))}
+        for index in range (len(algo.monteCarloSampler.indices)):
+            algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter] = get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter])
+            sample_counter = get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter]._sampleCounter)
+            S1 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[0][0]))
+            S2 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[1][0]))
+            S3 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[2][0]))
+            S4 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[3][0]))
+            S5 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[4][0]))
+            S6 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[5][0]))
+            S7 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[6][0]))
+            S8 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[7][0]))
+            S9 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[8][0]))
+            S10 = float(get_value_from_remote(algo.monteCarloSampler.indices[index].qoiEstimator[qoi_counter].powerSums[9][0]))
+            h1 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderOneDimensionZero(S1,sample_counter)))
+            h2 = float(get_value_from_remote(mdccm.computeCentralMomentsOrderTwoDimensionZeroBiased(S1,S2,sample_counter)))
+            qoi_dict["qoi_id_"+str(qoi_counter)]["index_"+str(index)] = {"qoi_id":qoi_counter, "index": index, "instances": sample_counter, "S1": S1, "S2": S2, "S3": S3, "S4": S4, "S5": S5, "S6": S6, "S7": S7, "S8": S8, "S9": S9, "S10": S10, "h1": h1, "h2": h2,"type":"time_series_quantity","tag":"base_moment_z"}
 
     # save time series pressure field
     for node in current_model.GetModelPart(model_part_of_interest).Nodes:
