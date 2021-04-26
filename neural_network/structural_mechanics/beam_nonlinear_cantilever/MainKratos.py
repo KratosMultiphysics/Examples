b8509121ee981609438e0c03b9c993f2a66bcc6a
danielandresarcones <daniel.andres.arcones@gmail.com>
2021-04-26 10:04:35 +0200

    Added the model formulation example in the non-linear cantilever beam.

--- neural_network/structural_mechanics/beam_nonlinear_cantilever/MainKratos.py
+++ neural_network/structural_mechanics/beam_nonlinear_cantilever/MainKratos.py
@@ -4,8 +4,10 @@ import KratosMultiphysics as KM
 from KratosMultiphysics.NeuralNetworkApplication.neural_network_analysis import NeuralNetworkAnalysis
 
 if __name__ == "__main__":
-
-    with open("ProjectParameters.json") as json_file:
+    # ProjectParametersWithModel generates a simple model without specifying the composition
+    with open("ProjectParametersWithModel.json") as json_file:
+    # ProjectParametersWithoutModel specifies the model in the json model
+    #with open("ProjectParametersWithoutModel.json") as json_file:
         #parameters = json.load(json_file)
         parameters = KM.Parameters(json_file.read())
 
