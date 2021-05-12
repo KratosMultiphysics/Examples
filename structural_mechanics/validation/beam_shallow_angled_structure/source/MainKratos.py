e7739d4065ead5b3d2f4d507ac5896955334f099
danielandresarcones <daniel.andres.arcones@gmail.com>
2021-05-12 11:27:36 +0200

    Actualized examples and modified beam_shallow_angled_structure to the current structure.

--- structural_mechanics/validation/beam_shallow_angled_structure/source/MainKratos.py
+++ structural_mechanics/validation/beam_shallow_angled_structure/source/MainKratos.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 from KratosMultiphysics import *
-from KratosMultiphysics.ExternalSolversApplication  import *
+from KratosMultiphysics.LinearSolversApplication  import *
 from KratosMultiphysics.StructuralMechanicsApplication  import *
 
 ## Import define_output
