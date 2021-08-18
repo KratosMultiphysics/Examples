96c5f2c9f84a1d46b78f10e971a24ee87cc862fc
Daniel Andres <daniel.andres.arcones@gmail.com>
2021-08-18 14:38:36 +0200

    Added fluid dynamics validation.

--- co_simulation/CoSim_DevExamples/pure_sdof/MainCoSim.py
+++ co_simulation/CoSim_DevExamples/pure_sdof/MainCoSim.py
@@ -2,16 +2,16 @@ from __future__ import print_function, absolute_import, division  # makes Kratos
 
 # Importing the Kratos Library => only needed to get the CoSim-Python-Scripts on the path!
 import KratosMultiphysics
-import KratosMultiphysics.EmpireApplication
+import KratosMultiphysics.CoSimulationApplication
 
 # Importing the base class
-from co_simulation_analysis import CoSimulationAnalysis
+from KratosMultiphysics.CoSimulationApplication.co_simulation_analysis import CoSimulationAnalysis
 import json
 
 parameter_file_name = "project_parameters_cosim_pure_SDof.json"
 
 with open(parameter_file_name, 'r') as parameter_file:
-    cosim_parameters = json.load(parameter_file)
+    cosim_parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
 simulation = CoSimulationAnalysis(cosim_parameters)
 simulation.Run()
--- co_simulation/CoSim_DevExamples/pure_sdof/PlotResults.py
+++ co_simulation/CoSim_DevExamples/pure_sdof/PlotResults.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 
 X, Y = [], []
-for line in open('results_sdof.txt', 'r'):
-  values = [float(s) for s in line.split()]
-  X.append(values[0])
-  Y.append(values[1])
+
+with open('sdof_solver//results_sdof.dat', 'r') as file:
+  reader = file.readlines()
+  for line in reader[1:]:
+    values = [float(s) for s in line.split()]
+    X.append(values[0])
+    Y.append(values[1])
 
 plt.plot(X, Y)
 plt.show()
