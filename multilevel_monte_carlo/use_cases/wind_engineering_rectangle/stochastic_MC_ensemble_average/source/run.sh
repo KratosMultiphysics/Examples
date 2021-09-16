66ab509c0826887f2fd172491d4d6985689960d3
riccardotosi <rtosi@cimne.upc.edu>
2021-09-16 13:09:54 +0200

    Remove since we are using a new single folder

--- multilevel_monte_carlo/use_cases/wind_engineering_rectangle/stochastic_MC_ensemble_average/source/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-runcompss \
-    --lang=python \
-    --python_interpreter=python3 \
-    --pythonpath=/PATH/TO/THIS/FOLDER/ \
-    ./run_mc_Kratos.py problem_settings/parameters_xmc_asynchronous_mc_rectangle_cylinder_Fractional.json
