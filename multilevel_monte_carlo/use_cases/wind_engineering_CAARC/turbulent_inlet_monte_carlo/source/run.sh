73a57b35d6287357bc064934e3ee6117d32f768a
riccardotosi <rtosi@cimne.upc.edu>
2021-09-16 15:45:05 +0200

    Improve CAARC turbulent inlet

--- multilevel_monte_carlo/use_cases/wind_engineering_CAARC/turbulent_inlet_monte_carlo/source/run.sh
+++ multilevel_monte_carlo/use_cases/wind_engineering_CAARC/turbulent_inlet_monte_carlo/source/run.sh
@@ -4,4 +4,4 @@ runcompss \
     --lang=python \
     --python_interpreter=python3 \
     --pythonpath=/PATH/TO/THIS/FOLDER/ \
-    ./run_mc_Kratos.py problem_settings/parameters_xmc_asynchronous_mc_CAARC3d_Fractional.json
+    ./run_mc_Kratos.py problem_settings/parameters_xmc.json
