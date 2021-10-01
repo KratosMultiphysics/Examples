6855cc7f2f8f1321de9c8a35fdf429fac1a20631
riccardotosi <rtosi@cimne.upc.edu>
2021-10-01 11:38:25 +0200

    Working with compss

--- multilevel_monte_carlo/validation/elliptic_benchmark/synchronous_multilevel_monte_carlo/run_with_pycompss.sh
+++ multilevel_monte_carlo/validation/elliptic_benchmark/synchronous_multilevel_monte_carlo/run_with_pycompss.sh
@@ -22,11 +22,14 @@ mdpa_new_path_7="$path_to_examples_folder/../problem_settings/square_level_7"
 mdpa_path_to_replace_8="../problem_settings/square_level_8"
 mdpa_new_path_8="$path_to_examples_folder/../problem_settings/square_level_8"
 
+mdpa_path_to_replace=( $mdpa_path_to_replace_1 $mdpa_path_to_replace_2 $mdpa_path_to_replace_3 $mdpa_path_to_replace_4 $mdpa_path_to_replace_5 $mdpa_path_to_replace_6 $mdpa_path_to_replace_7 $mdpa_path_to_replace_8 )
+mdpa_new_path=( $mdpa_new_path_1 $mdpa_new_path_2 $mdpa_new_path_3 $mdpa_new_path_4 $mdpa_new_path_5 $mdpa_new_path_6 $mdpa_new_path_7 $mdpa_new_path_8 )
+
 # set absolute path in Kratos parameters
-for i in 1 2 3 4 5 6 7 8
+for i in {1..8}
 do
     sed -i "s|$materials_path_to_replace_poisson|$materials_new_path_poisson|g" "../problem_settings/parameters_level_$i.json"
-    sed -i "s|$mdpa_path_to_replace_$i|$mdpa_new_path_$i|g" "../problem_settings/parameters_level_$i.json"
+    sed -i "s|${mdpa_path_to_replace[i-1]}|${mdpa_new_path[i-1]}|g" "../problem_settings/parameters_level_$i.json"
 done
 
 runcompss \
@@ -36,8 +39,8 @@ runcompss \
     ./run_mlmc_Kratos.py
 
 # revert change in Kratos parameters
-for i in 1 2 3 4 5 6 7 8
+for i in {1..8}
 do
     sed -i "s|$materials_new_path_poisson|$materials_path_to_replace_poisson|g" "../problem_settings/parameters_level_$i.json"
-    sed -i "s|$mdpa_new_path_$i|$mdpa_path_to_replace_$i|g" "../problem_settings/parameters_level_$i.json"
+    sed -i "s|${mdpa_new_path[i-1]}|${mdpa_path_to_replace[i-1]}|g" "../problem_settings/parameters_level_$i.json"
 done
