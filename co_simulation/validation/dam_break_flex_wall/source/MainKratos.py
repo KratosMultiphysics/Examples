335d4acc7cdb7115c8a7d5b862714041d4813d8b
ceulogiof <ceuflores11@gmail.com>
2021-12-11 01:37:53 +0100

    Deleted logger from MainKratos.py

--- co_simulation/validation/dam_break_flex_wall/source/MainKratos.py
+++ co_simulation/validation/dam_break_flex_wall/source/MainKratos.py
@@ -11,14 +11,5 @@ parameter_file_name = "ProjectParametersCoSim.json"
 with open(parameter_file_name,'r') as parameter_file:
     parameters = KM.Parameters(parameter_file.read())
 
-#TODO - Remove loggers before merging to master.
-file_logger_info = KM.FileLoggerOutput("KratosInfo.log")
-file_logger_info.SetSeverity(KM.Logger.Severity.INFO)
-KM.Logger.AddOutput(file_logger_info)
-
-file_logger_warn = KM.FileLoggerOutput("KratosWarning.log")
-file_logger_warn.SetSeverity(KM.Logger.Severity.WARNING)
-KM.Logger.AddOutput(file_logger_warn)
-
 simulation = CoSimulationAnalysis(parameters)
 simulation.Run()
