be9aa3b30107ce9bc5a2e7d0a79701e20cf6af26
Armin Geiser <armin.geiser@tum.de>
2022-01-04 12:19:06 +0100

    Shapeopt/update for kratos 9 (#97)
    * update examples for Kratos version 9.0
    
    * clean python scripts

--- shape_optimization/use_cases/09_Packaging_3D_Shell/run_optimization.py
+++ shape_optimization/use_cases/09_Packaging_3D_Shell/run_optimization.py
@@ -1,12 +1,9 @@
-# Making KratosMultiphysics backward compatible with python 2.6 and 2.7
-from __future__ import print_function, absolute_import, division
-
 # Import Kratos core and apps
 import KratosMultiphysics as KM
 
 # Additional imports
 from KratosMultiphysics.ShapeOptimizationApplication import optimizer_factory
-from KratosMultiphysics.ShapeOptimizationApplication.analyzer_base import AnalyzerBaseClass
+from KratosMultiphysics.ShapeOptimizationApplication.analyzers.analyzer_base import AnalyzerBaseClass
 
 # Read parameters
 with open("optimization_parameters.json",'r') as parameter_file:
