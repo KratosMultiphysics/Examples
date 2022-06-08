0d17fabbd99a8d9a2e4bfcfe8279d45ae61b1793
migelmaso <mmaso@cimne.upc.edu>
2022-06-08 12:13:46 +0200

    job launcher

--- shallow_water/validation/mac_donald_shock/source/MainKratos.py
+++ shallow_water/validation/mac_donald_shock/source/MainKratos.py
@@ -6,37 +6,34 @@ from KratosMultiphysics.ShallowWaterApplication.shallow_water_analysis import Sh
 from KratosMultiphysics.ShallowWaterApplication.utilities import benchmarking_utilities as utils
 
 parser = argparse.ArgumentParser()
-mode = parser.add_mutually_exclusive_group()
-mode.add_argument("-r", "--regular_analysis", action="store_const", dest="mode", const="regular_analysis", default="regular_analysis")
-mode.add_argument("-c", "--convergence_analysis", action="store_const", dest="mode", const="convergence_analysis")
+parser.add_argument("--time_step", type=float)
+parser.add_argument("--courant_number", type=float)
+parser.add_argument("--automatic_time_step", type=bool)
+parser.add_argument("--shock_capturing_type", type=str)
+parser.add_argument("--input_filename", type=str)
+parser.add_argument("--analysis_label", type=str)
+parser.add_argument("--remove_output_processes", type=bool, default=False)
 args = parser.parse_args()
+
+print(args)
 
 with open("ProjectParameters.json",'r') as parameter_file:
     parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-if args.mode == "regular_analysis":
-    model = KratosMultiphysics.Model()
-    ShallowWaterAnalysis(model, parameters).Run()
-else:
-    modes = ['residual_viscosity','gradient_jump','flux_correction']
-    labels = ['rv','gj','fc']
-    meshes = [2.0, 1.0, 0.5, 0.2, 0.1]
-    steps = [0.005] * len(meshes)
-    steps[-1] = 0.002
-    input_base_name = 'mac_donald_{}'
-    for mode, label in zip(modes, labels):
-        for mesh, time_step in zip(meshes, steps):
-            case = parameters.Clone()
-            input_name = input_base_name.format(mesh)
-            case['solver_settings']['time_stepping']['automatic_time_step'].SetBool(True)
-            case['solver_settings']['time_stepping']['courant_number'].SetDouble(0.5)
-            case['solver_settings']['time_stepping']['time_step'].SetDouble(time_step)
-            case['solver_settings']['shock_capturing_type'].SetString(mode)
-            utils.GetModelerParameters(case['modelers'], 'import_mdpa_modeler')['input_filename'].SetString(input_name)
-            utils.GetProcessParameters(case['output_processes'], 'convergence_output_process')['analysis_label'].SetString(label)
-            utils.KeepOnlyThisProcess(case['output_processes'], 'convergence_output_process')
-            model = KratosMultiphysics.Model()
-            try:
-                ShallowWaterAnalysis(model, case).Run()
-            except RuntimeError:
-                pass
+if args.time_step is not None:
+    parameters['solver_settings']['time_stepping']['time_step'].SetDouble(args.time_step)
+if args.courant_number is not None:
+    parameters['solver_settings']['time_stepping']['courant_number'].SetDouble(args.courant_number)
+if args.automatic_time_step is not None:
+    parameters['solver_settings']['time_stepping']['automatic_time_step'].SetBool(args.automatic_time_step)
+if args.shock_capturing_type is not None:
+    parameters['solver_settings']['shock_capturing_type'].SetString(args.shock_capturing_type)
+if args.input_filename is not None:
+    utils.GetModelerParameters(parameters['modelers'], 'import_mdpa_modeler')['input_filename'].SetString(args.input_filename)
+if args.analysis_label is not None:
+    utils.GetProcessParameters(parameters['output_processes'], 'convergence_output_process')['analysis_label'].SetString(args.analysis_label)
+if args.remove_output_processes:
+    utils.KeepOnlyThisProcess(parameters['output_processes'], 'convergence_output_process')
+
+model = KratosMultiphysics.Model()
+ShallowWaterAnalysis(model, parameters).Run()
---
+++ shallow_water/validation/mac_donald_shock/source/job_launcher.py
@@ -0,0 +1,26 @@
+import os
+
+automatic_time_step = True
+courant_number = 0.5
+modes = ['residual_viscosity','gradient_jump','flux_correction']
+labels = ['rv','gj','fc']
+meshes = [2.0, 1.0, 0.5, 0.2, 0.1]
+steps = [0.005] * len(meshes)
+steps[-1] = 0.002
+input_filename_pattern = 'mac_donald_{}'
+
+count = 0
+for mode, label in zip(modes, labels):
+    for mesh, time_step in zip(meshes, steps):
+        count += 1
+        input_filename = input_filename_pattern.format(mesh)
+        job_name = f'mac_donald_conv_analysis_{count}'
+        command  = f'sbatch --job-name {job_name} run.sh'
+        command += f' --automatic_time_step {automatic_time_step}'
+        command += f' --courant_number {courant_number}'
+        command += f' --time_step {time_step}'
+        command += f' --shock_capturing_type {mode}'
+        command += f' --input_filename {input_filename}'
+        command += f' --analysis_label {label}'
+        command += f' --remove_output_processes True'
+        os.system(command=command)
