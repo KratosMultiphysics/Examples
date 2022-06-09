1c341bc8050117b82f6327c170edb849fa9bbdfb
migelmaso <mmaso@cimne.upc.edu>
2022-06-09 14:11:20 +0200

    safer parallel run

--- shallow_water/validation/mac_donald_shock/source/MainKratos.py
+++ shallow_water/validation/mac_donald_shock/source/MainKratos.py
@@ -11,6 +11,7 @@ parser.add_argument("--courant_number", type=float)
 parser.add_argument("--automatic_time_step", type=bool)
 parser.add_argument("--shock_capturing_type", type=str)
 parser.add_argument("--input_filename", type=str)
+parser.add_argument("--output_filename", type=str)
 parser.add_argument("--analysis_label", type=str)
 parser.add_argument("--remove_output", type=bool, default=False)
 args = parser.parse_args()
@@ -30,6 +31,8 @@ if args.shock_capturing_type is not None:
     parameters['solver_settings']['shock_capturing_type'].SetString(args.shock_capturing_type)
 if args.input_filename is not None:
     utils.GetModelerParameters(parameters['modelers'], 'import_mdpa_modeler')['input_filename'].SetString(args.input_filename)
+if args.output_filename is not None:
+    utils.GetProcessParameters(parameters['output_processes'], 'convergence_output_process')['file_name'].SetString(args.output_filename)
 if args.analysis_label is not None:
     utils.GetProcessParameters(parameters['output_processes'], 'convergence_output_process')['analysis_label'].SetString(args.analysis_label)
 if args.remove_output:
