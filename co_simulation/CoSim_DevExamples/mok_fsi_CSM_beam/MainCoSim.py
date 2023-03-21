d30e4583392182508549461bc5ad01af395e62b6
Vicente Mataix Ferrándiz <vmataix@altair.com>
2023-03-21 11:22:37 +0100

    Cleaning scripts

--- co_simulation/CoSim_DevExamples/mok_fsi_CSM_beam/MainCoSim.py
+++ co_simulation/CoSim_DevExamples/mok_fsi_CSM_beam/MainCoSim.py
@@ -1,4 +1,3 @@
-from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 
 # Importing the Kratos Library => only needed to get the CoSim-Python-Scripts on the path!
 import KratosMultiphysics
