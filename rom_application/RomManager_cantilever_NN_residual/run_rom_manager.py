3161c1932c372a69ae7a935a71cb27e2f190bdbd
NicolasSR <54904691+NicolasSR@users.noreply.github.com>
2025-05-15 16:55:06 +0200

    Corrected finetuning of NN via residual

--- rom_application/RomManager_cantilever_NN_residual/run_rom_manager.py
+++ rom_application/RomManager_cantilever_NN_residual/run_rom_manager.py
@@ -41,7 +41,7 @@ def UpdateProjectParameters(parameters, mu=None):
     """
     Customize ProjectParameters here for imposing different conditions to the simulations as needed
     """
-    steps = 300
+    steps = 400
     parameters["processes"]["loads_process_list"][0]["Parameters"]["modulus"].SetString(str(mu[0]/steps)+"*t")
     parameters["processes"]["loads_process_list"][1]["Parameters"]["modulus"].SetString(str(mu[1]/steps)+"*t")
     parameters["problem_data"]["end_time"].SetDouble(steps)
@@ -76,7 +76,7 @@ def GetRomManagerParameters():
                 "rom_basis_output_format": "numpy",                         
                 "rom_basis_output_name": "RomParameters",
                 "snapshots_control_type": "time",                          // "step", "time"
-                "snapshots_interval": 300,
+                "snapshots_interval": 400,
                 "snapshots_control_is_periodic": false,
                 "print_singular_values": true,
                 "galerkin_rom_bns_settings": {
@@ -94,7 +94,7 @@ def GetRomManagerParameters():
                     "modes":[14,60],
                     "layers_size":[200,200],
                     "batch_size":16,
-                    "epochs":800,
+                    "epochs":6,
                     "NN_gradient_regularisation_weight": 0.0,
                     "lr_strategy":{
                         "scheduler": "sgdr",
@@ -134,33 +134,45 @@ if __name__ == "__main__":
     
     mu_train = np.array(get_multiple_params(500, 824)).tolist()
     mu_validation = np.array(get_multiple_params(100, 235)).tolist()
-    mu_test = np.array(get_multiple_params(100, 539)).tolist()
+    mu_test = np.array(get_multiple_params(10, 539)).tolist()
+    # mu_test = np.array(get_multiple_params(100, 539)).tolist()
 
     general_rom_manager_parameters = GetRomManagerParameters()
     project_parameters_name = "datasets_rubber_hyperelastic_cantilever_big_range/ProjectParameters_FOM.json"
 
     rom_manager = RomManager(project_parameters_name,general_rom_manager_parameters,CustomizeSimulation,UpdateProjectParameters, UpdateMaterialParametersFile)
 
-    # First, train the snapshot-based model
+    # First, train the ANN-PROM model via snapshot-based loss
     rom_manager.Fit(mu_train=mu_train, mu_validation=mu_validation)
-    
-    # rom_manager.Test(mu_test=mu_test, mu_train=mu_train, start_from_closest_mu=False, filter_nan=True)
-    # rom_manager.PrintErrors()
-    # rom_manager.TestNeuralNetworkReconstruction(mu_train, mu_validation, mu_test)
-    # snapshots_matrix = rom_manager.GenerateOrderedFOMSnapshotsMatrix(mu_train)
-    # print(snapshots_matrix.shape)
-    # np.save('mu_train.npy', mu_train)
-    # np.save('snapshots_train.npy', snapshots_matrix.T)
 
-    
-    # mu_run_raw = np.array(get_multiple_params(1, 97))
-    # mu_run = mu_run_raw
-    # mu_run = mu_run.tolist()
-    # print(mu_run)
-    # rom_manager.Test(mu_test=mu_run, mu_train=mu_train, start_from_closest_mu=False, filter_nan=True)
-    # rom_manager.PrintErrors()
-    # rom_manager.RunFOM(mu_run)
-    # rom_manager.RunROM(mu_run, mu_train)
+    # Then, train the ANN-PROM model via snapshot-based loss on top of one we just trained.
+    in_database, sloss_model_name = rom_manager.data_base.check_if_in_database("Neural_Network", mu_train)
 
+    assert in_database
+    print(sloss_model_name)
+
+    # Update the ROM Manager parameters to include the recently-trained ANN's directory
+    pretrained_model_path = str(rom_manager.data_base.database_root_directory)+'/saved_nn_models/' + sloss_model_name
+    rom_manager.general_rom_manager_parameters["ROM"]["ann_enhanced_settings"]["online"]["custom_model_path"].SetString(pretrained_model_path)
+    rom_manager.general_rom_manager_parameters["ROM"]["ann_enhanced_settings"]["lr_strategy"]["base_lr"].SetDouble(0.0001)
+    print(rom_manager.general_rom_manager_parameters)
+
+    # Finetune the NN using the residual loss
+    rom_manager.FinetuneANNOnResidual(mu_train, mu_validation)
+
+    # Test accuracy trained on snapshot
+    rom_manager.general_rom_manager_parameters["ROM"]["ann_enhanced_settings"]["lr_strategy"]["base_lr"].SetDouble(0.001)
+    rom_manager.Test(mu_test=mu_test, mu_train=mu_train, filter_nan=True)
+    rom_manager.PrintErrors()
+
+    # Test accuracy finetuned on residual
+    in_database, rloss_model_name = rom_manager.data_base.check_if_in_database("Neural_Network_Residual", mu_train)
+    assert in_database
+    print(rloss_model_name)
+    finetuned_model_path = str(rom_manager.data_base.database_root_directory)+'/saved_nn_models_residual/' + rloss_model_name
+    rom_manager.general_rom_manager_parameters["ROM"]["ann_enhanced_settings"]["online"]["custom_model_path"].SetString(finetuned_model_path)
+    rom_manager.general_rom_manager_parameters["ROM"]["ann_enhanced_settings"]["lr_strategy"]["base_lr"].SetDouble(0.0001)
+    rom_manager.Test(mu_test=mu_test, mu_train=mu_train, filter_nan=True)
+    rom_manager.PrintErrors()
 
 
