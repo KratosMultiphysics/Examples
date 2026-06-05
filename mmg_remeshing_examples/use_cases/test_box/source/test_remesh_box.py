2087848b26626d22c8994bd39524d6a9c531af02
Vicente Mataix Ferrándiz <vmataix@altair.com>
2026-06-05 13:56:27 +0200

    Minor

--- mmg_remeshing_examples/use_cases/test_box/source/test_remesh_box.py
+++ mmg_remeshing_examples/use_cases/test_box/source/test_remesh_box.py
@@ -1,6 +1,3 @@
-# We import the libraies
-from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-
 # Import Kratos
 import KratosMultiphysics
 import KratosMultiphysics.MeshingApplication as MeshingApplication
